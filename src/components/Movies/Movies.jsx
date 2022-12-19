c889eda8f568d09d3fc3bbb4ddcae51e7844d031
ViswaPrasanth <85486741+ViswaPrashanth@users.noreply.github.com>
2022-12-19 18:30:17 +0530

    added search functionality

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -12,12 +12,12 @@ import { MovieList } from "../index";
 
 const Movies = () => {
   const [page, setPage] = useState(1);
-  const { genreIdOrCategoryName } = useSelector(
+  const { genreIdOrCategoryName, searchQuery } = useSelector(
     (state) => state.currentGenreOrCategory
   );
   const { data, error, isFetching } = useGetMoviesQuery({
     genreIdOrCategoryName,
-    page,
+    page, searchQuery
   });
 
   if (isFetching) {
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
-const page = 1;
 
 // https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 
@@ -16,7 +15,12 @@ export const tmdbApi = createApi({
 
     // Get movies by [Type]
     getMovies: builder.query({
-      query: ({ genreIdOrCategoryName, page }) => {
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        //Get movies by search
+        if (searchQuery) {
+          return `search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
         //Get movies by categories
         if (
           genreIdOrCategoryName &&
