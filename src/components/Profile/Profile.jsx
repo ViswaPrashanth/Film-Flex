bb605b5ba977bf20e924992a38b1bcc47a3b3739
ViswaPrasanth <85486741+ViswaPrashanth@users.noreply.github.com>
2022-12-20 12:27:11 +0530

    added tmdb  Authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Typography, Button, Box } from "@mui/material";
+import { useSelector } from "react-redux";
+import { ExitToApp } from "@mui/icons-material";
+
+import { userSelector } from "../../features/auth";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { user } = useSelector(userSelector);
+  const favMovies = [];
+  const logout = () => {
+    localStorage.clear();
 
-export default Profile
+    window.location.href = "/";
+  };
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />{" "}
+        </Button>
+      </Box>
+      {!favMovies.length ? (
+        <Typography variant="h5">
+          Add favourites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>Favourite Movies </Box>
+      )}
+    </Box>
+  );
+};
+
+export default Profile;
